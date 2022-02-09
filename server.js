050cfa1b3fd81ec5e143befa089bb110218acd64
Yousif Shafee <yousiftariq1@hotmail.com>
2022-02-09 15:22:50 +0200

    Request Header Parser Microservice

--- server.js
+++ server.js
@@ -21,8 +21,11 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/whoami", function (req, res) {
+  var ipaddress = req.socket.remoteAddress;
+  var lang = req.headers['accept-language']
+  var software = req.headers['user-agent']
+  res.json({ ipaddress: ipaddress, language: lang, software: software });
 });
 
 
